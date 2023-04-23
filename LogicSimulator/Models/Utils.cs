09cb77d419c17ec8f66d2a14698785e0cfe28b83
Вадим Борисов <98724575+Kirpitchnsk@users.noreply.github.com>
2023-04-23 23:46:21 +0700

    Полностью окончена разработка лаунчерного меню! ;'-}

--- LogicSimulator/Models/Utils.cs
+++ LogicSimulator/Models/Utils.cs
@@ -677,6 +677,15 @@ namespace LogicSimulator.Models {
             var bounds2 = tb2.Value.Bounds.TransformToAABB(tb2.Value.Transform);
             return res - bounds2.TopLeft;
         }
+
+        public static DateTime UnixTimeStampToDateTime(this long unixTimeStamp) {
+            DateTime dateTime = new(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+        public static string UnixTimeStampToString(this long unixTimeStamp) {
+            return UnixTimeStampToDateTime(unixTimeStamp).ToString("yyyy/mm/dd H:mm:ss");
+        }
         
         /*
          * SQLite_proj_list абилка
